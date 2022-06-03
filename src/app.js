ecaaa00b0b674c3915de7875cd7743e04719a5f3
James-S-007 <jspringer8@gatech.edu>
2022-06-03 10:08:10 -0400

    Move css out of js

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux'
 import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore'
 import { addExpense } from './actions/expenses'
-import { setTextFilter } from './actions/filters'
 import getVisibleExpeneses from './selectors/expenses'
 import 'normalize.css/normalize.css'  // cross-browser view
 import './styles/styles.scss'
@@ -12,13 +11,7 @@ import 'react-dates/lib/css/_datepicker.css'
 
 const store = configureStore()
 
-store.dispatch(addExpense({ description: 'Water bill', amount: 300, createdAt: 123}))
-store.dispatch(addExpense({ description: 'Gas bill', amount: 5134, createdAt: 4000}))
-store.dispatch(addExpense({ description: 'Rent', amount: 122700, createdAt: 1000}))
-
-
 const state = store.getState()
-console.log(getVisibleExpeneses(state.expenses, state.filters))
 
 const jsx = (
     <Provider store={store}>
