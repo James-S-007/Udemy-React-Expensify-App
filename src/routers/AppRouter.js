2a9695557e179d125c726d6ae8d7ca29ff0dbb1e
James-S-007 <jspringer8@gatech.edu>
2022-06-06 12:28:32 -0400

    Store expenses data under each user and auth validation

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,9 +4,9 @@ import { createBrowserHistory } from 'history'
 import AddExpensePage from '../components/AddExpensePage'
 import EditExpensePage from '../components/EditExpensePage'
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage'
-import HelpPage from '../components/HelpPage'
 import LoginPage from '../components/LoginPage'
 import NotFoundPage from '../components/NotFoundPage'
+import PublicRoute from './PublicRoute'
 import PrivateRoute from './PrivateRoute'
 
 export const history = createBrowserHistory()
@@ -14,11 +14,10 @@ export const history = createBrowserHistory()
 const AppRouter = () => (
     <Router history={history}>
         <Switch>
-            <Route path='/' component={LoginPage} exact={true} />
+            <PublicRoute path='/' component={LoginPage} exact={true} />
             <PrivateRoute path='/dashboard' component={ExpenseDashboardPage} />
             <PrivateRoute path='/create' component={AddExpensePage} />
             <PrivateRoute path='/edit/:id' component={EditExpensePage} />
-            <Route path='/help' component={HelpPage} />
             <Route component={NotFoundPage} />
         </Switch>
     </Router>
